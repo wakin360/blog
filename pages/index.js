96ddcd5edf99172008dc17e7a925e052cf73c87b
wakin <jmmanalastas.student@ua.edu.ph>
2026-08-20 21:57:31 +0800

    Updated project files

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am a 3rd year college student studying Information Technology. My hobbies include gaming, reading, and listening to music. I especially enjoy video game soundtracks because they help me focus and relax.
+
+My favorite food is fried chicken, which I always enjoy as a comfort meal.
+
+In the next five years, I see myself working in a job that may not be connected to my current course, but I am open to learning new skills and finding the right path for me.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
